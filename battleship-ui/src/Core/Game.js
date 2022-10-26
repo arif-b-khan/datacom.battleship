0da7ff827fdff85ab85b0d5fb7e4144263e51e82
Arif Khan <arif.bannehasan@gmail.com>
2022-10-26 21:23:09 +0530

    Adding starter project
    #### Description
    - Performed cleanup
    - adding tailwind css

---
+++ battleship-ui/src/Core/Game.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Game() {
+    return (
+        <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </div >
+    );
+}
+
+export default Game;
